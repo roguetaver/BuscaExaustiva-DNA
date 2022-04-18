f0cea376db8949bc21d9666effedc26e86b18400
roguetaver <andretavernaro@gmail.com>
2022-04-18 14:13:39 -0300

    init

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}
+
+// g++ -Wall -O3 main.cpp -o main
+// ./main < dna.seq
